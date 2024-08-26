275ceaa9f33b817920ba2ea8e59fcadd3c4e0063
xbgm08 <gabrielbraulino@gmail.com>
2024-08-25 23:02:30 -0300

    feat: adiciona um terminal interativo para criar conta no banco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Bem-vindo ao terminal de contas!");
+        System.out.println("Por favor, digite seu nome:");
+        String nome = scanner.next();
+        
+        System.out.println("Por favor, digite sua agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da conta:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o seu saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e o seu saldo " + saldo + " já está disponível para saque.");
     }
 }
